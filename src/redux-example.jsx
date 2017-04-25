0592d12acda60acdac7dc4b604a4edeba238df8b
Matt C <Matt@journeysoft.com>
2017-04-25 16:49:07 -0400

    init

--- src/redux-example.jsx
+++ src/redux-example.jsx
@@ -1,3 +1,23 @@
 var redux = require('redux');
 
 console.log('Starting redux example');
+
+function changeProp(obj) {
+    return {
+        ...obj, 
+        name: 'Maria'
+    };
+}
+
+
+var startingValue = {
+    name: 'Matteo',
+    age: 33
+
+};
+
+var res = changeProp(startingValue);
+
+console.log ("startingValue", startingValue);
+
+console.log ("res", res);
